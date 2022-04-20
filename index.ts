1480f6f912146b71300524ffcdf8018d0fbd54e8
CarlaCapelli <92063946+CarlaCapelli@users.noreply.github.com>
2022-04-19 22:11:22 -0300

    animacion

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 let parDos = document.getElementById("p-2");
 let btn = document.getElementById("boton");
-
+let btn2 = document.getElementById("boton2");
 btn?.addEventListener("click", function () {
   this.classList.toggle("ocultar");
   parDos?.classList.toggle("ocultar");
+  btn2?.classList.toggle("ocultar");
+});
+btn2?.addEventListener("click", function () {
+  this.classList.toggle("ocultar");
+  parDos?.classList.toggle("ocultar");
+  btn?.classList.toggle("ocultar");
 });
